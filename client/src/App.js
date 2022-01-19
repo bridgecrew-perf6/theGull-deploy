a91d63bc1cce90b67b6d215570ea8f1fcd7a27d7
Muammer Necipoglu <muammernecipoglu@gmail.com>
2022-01-18 23:28:44 -0800

    Add Header component and style it for Desktop view

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import Header from "./components/Header/Header";
+import { Routes, Route } from "react-router-dom";
 import "./styles/App.scss";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<h1>Welcome to The Gull</h1>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,52 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/logo/logo.png";
+import cart from "../../assets/icons/cart.svg";
 import "./Header.scss";
 
 export default function Header() {
+  const [mode, setMode] = useState("dark");
   return (
-    <header>
-      <img src={logo} alt="Logo" />
+    <header className="header">
+      <nav className="header__nav">
+        <Link to="/">
+          <img src={logo} alt="Logo" />
+        </Link>
+        <ul className="header__nav-list">
+          <li>
+            <NavLink
+              to="/shop"
+              className={({ isActive }) =>
+                "header__nav-item" +
+                (isActive ? " header__nav-item--active" : "")
+              }
+            >
+              Shop
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/signin"
+              className={({ isActive }) =>
+                "header__nav-item" +
+                (isActive ? " header__nav-item--active" : "")
+              }
+            >
+              Sign in
+            </NavLink>
+          </li>
+          <li>
+            <Link to="/cart" className="header__nav-item">
+              <img src={cart} alt="shopping cart" />
+            </Link>
+          </li>
+        </ul>
+        <span
+          className={`header__mode ${
+            mode === "dark" ? "header__mode--dark" : "header__mode--dark"
+          }`}
+        />
+      </nav>
     </header>
   );
 }
