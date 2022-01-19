9f38d19474eb2e8a6493466e52336b66cdb02f98
Muammer Necipoglu <muammernecipoglu@gmail.com>
2022-01-18 21:19:59 -0800

    Initial project setup

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import Header from "./components/Header/Header";
+import "./styles/App.scss";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import logo from "../../assets/logo/logo.png";
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="Logo" />
+    </header>
+  );
+}
