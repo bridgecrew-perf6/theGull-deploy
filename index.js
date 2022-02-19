72f354389de775df098ff21c71b5a906cc2805ce
Muammer Necipoglu <muammernecipoglu@gmail.com>
2022-02-18 23:59:23 -0800

    Fix pathname for front-end

--- index.js
+++ index.js
@@ -14,21 +14,21 @@ const User = require("./data/User");
 const cookieParser = require("cookie-parser");
 
 const {
-    PORT,
-    CLIENT_URL,
-    GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET,
-    MONGODB_URL,
+  PORT,
+  CLIENT_URL,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  MONGODB_URL,
 } = process.env;
 
 const app = express();
 
 mongoose.connect(
-    MONGODB_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-        console.log("mongoose connection successful");
-    }
+  MONGODB_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("mongoose connection successful");
+  }
 );
 
 app.use(cors({ origin: CLIENT_URL, credentials: true }));
@@ -37,80 +37,80 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 if (process.env.NODE_ENV === "production") {
-    app.use(enforce.HTTPS({ trustProtoHeader: true }));
-    app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
 app.use(
-    session({
-        secret: process.env.SESSION_SECRET,
-        resave: false,
-        saveUninitialized: true,
-        cookie: {
-            sameSite: "none",
-            secure: true,
-        },
-    })
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      sameSite: "none",
+      secure: true,
+    },
+  })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: GOOGLE_CLIENT_ID,
-            clientSecret: GOOGLE_CLIENT_SECRET,
-            callbackURL: "/auth/google/callback",
-        },
-        (accessToken, refreshToken, profile, cb) => {
-            User.findOne({ email: profile._json.email }, async (err, user) => {
-                if (err) return cb(err, null);
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, cb) => {
+      User.findOne({ email: profile._json.email }, async (err, user) => {
+        if (err) return cb(err, null);
 
-                if (!user) {
-                    const newUser = new User({
-                        username: profile.name.givenName,
-                        email: profile.emails[0].value,
-                        isAdmin: false,
-                    });
+        if (!user) {
+          const newUser = new User({
+            username: profile.name.givenName,
+            email: profile.emails[0].value,
+            isAdmin: false,
+          });
 
-                    await newUser.save();
-                    return cb(null, newUser);
-                }
-                return cb(null, user);
-            });
+          await newUser.save();
+          return cb(null, newUser);
         }
-    )
+        return cb(null, user);
+      });
+    }
+  )
 );
 
 passport.use(
-    new LocalStrategy((email, password, cb) => {
-        User.findOne({ email }, async (err, user) => {
-            if (err) throw err;
-            if (!user) return cb(null, false);
-            bcrypt.compare(password, user.password, (err, result) => {
-                if (err) throw err;
-                if (result) return cb(null, user);
-                else return cb(null, false);
-            });
-        });
-    })
+  new LocalStrategy((email, password, cb) => {
+    User.findOne({ email }, async (err, user) => {
+      if (err) throw err;
+      if (!user) return cb(null, false);
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) throw err;
+        if (result) return cb(null, user);
+        else return cb(null, false);
+      });
+    });
+  })
 );
 
 passport.serializeUser((user, cb) => {
-    return cb(null, user._id);
+  return cb(null, user._id);
 });
 
 // The return value is what we send to the client through req.user
 passport.deserializeUser((id, cb) => {
-    User.findById(id, (err, user) => {
-        if (!err) return cb(null, user);
-        return cb(err, null);
-    });
+  User.findById(id, (err, user) => {
+    if (!err) return cb(null, user);
+    return cb(err, null);
+  });
 });
 
 app.use("/auth", authRouter);
